c66491a47ddb61977ffaed1ae8ca50a9994c8c91
vince <sip94@tut.by>
2018-11-04 21:13:58 +0300

    firstTask

---
+++ Stairs/src/App.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+
+
+    //int  number;
+    int countStairs;
+
+    public static void main(String[] args) {
+        boolean flag = true;
+        ArrayList<Integer> stair = new ArrayList<>();
+        //Stair first = new Stair();
+        System.out.println("Enter your number");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        stair.add(number);
+
+
+        while (flag) {
+            int newStair = 0;
+            stair.add(newStair);
+
+            for (int i = 0; i < stair.size() ; i++) {
+
+                while (stair.get(i + 1) + 2 < stair.get(i)) {
+                    stair.set(i, stair.get(i) - 1);
+                    stair.set(i + 1, stair.get(i + 1) + 1);
+                }
+//                System.out.println(number);
+//                System.out.println(newStair);
+            }
+            if (stair.get(stair.size() - 1) == 0) {
+                flag = false;
+            }
+        }
+        for (int i = 0; i < stair.size(); i++) {
+            System.out.println(stair.get(i));
+        }
+    }
+}
