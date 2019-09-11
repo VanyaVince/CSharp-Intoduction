dd65ac4440e44459e6a6f96d3dec16251e056bf9
vince <sip94@tut.by>
2019-09-11 23:42:05 +0300

    c# basic 'addition'

---
+++ addition/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addition
+{
+    class Program
+    {
+        static int firstNumber;
+        static int secondNumber;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number");
+            firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number");
+            secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(firstNumber + " + " + secondNumber + " = " + (firstNumber + secondNumber));
+            
+
+        }
+
+        private static void addition (int a, int b)
+        {
+            Console.WriteLine(a + " + " + b + " = " + a + b);
+        }
+    }
+}
