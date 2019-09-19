6e36a19d21169ac7072da267b73ffcb08d370747
vince <vanyavince@gmail.com>
2019-09-19 12:42:12 +0300

    C# classes

---
+++ Classes(C_Sharp)/Classes(C_Sharp)/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Classes_C_Sharp_
+{
+    public class Person
+    {
+        public String firstName;
+        public String lastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hi, it's " + firstName + " " + lastName);
+        } 
+    };
+}
---
+++ Classes(C_Sharp)/Classes(C_Sharp)/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Classes_C_Sharp_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person vince = new Person();
+            vince.firstName = "Vince";
+            vince.lastName = "Carter";
+            vince.introduce();
+        }
+    }
+}
