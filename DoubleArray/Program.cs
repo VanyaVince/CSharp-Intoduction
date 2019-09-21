38df7a75432d385e15bca7a58b40c673f51bee8d
vince <vanyavince@gmail.com>
2019-09-21 18:23:20 +0300

    doubleArray

---
+++ DoubleArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array1 = new int[6];
+            var array2 = new int[6];
+
+            for (var i = 0; i < array1.Length; i++)
+            {
+                array1[i] = i + 1;
+                for (var j = 0; j < array2.Length; j++)
+                {
+                    array2[j] = j + 1;
+                    Console.Write(string.Format("{0}{1} ", array1[i], array2[j] ));
+                    if (j == 5)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
