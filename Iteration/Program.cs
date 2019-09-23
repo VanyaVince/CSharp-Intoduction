ce4c9f8e1a80f06c218eeaa1ad4b4d4339748cf4
vince <vanyavince@gmail.com>
2019-09-23 12:02:57 +0300

    Iteration Exercise1

---
+++ Iteration/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise1DividedBy3();
+        }
+        /*summary
+        display the count of all characters(numbers) that are divisible by 3 with no remainder */
+        private static void Exercise1DividedBy3()
+        {
+            var count = 0;
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                    count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
