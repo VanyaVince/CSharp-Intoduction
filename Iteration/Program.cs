992d5ce3d34487a82f50a7e72fb0f1c89d360024
vince <vanyavince@gmail.com>
2019-09-23 12:24:04 +0300

    Exercise2

--- Iteration/Program.cs
+++ Iteration/Program.cs
@@ -10,7 +10,8 @@ namespace Iteration
     {
         static void Main(string[] args)
         {
-            Exercise1DividedBy3();
+            //Exercise1DividedBy3();
+            //Exercise2SumNumbers();
         }
         /*summary
         display the count of all characters(numbers) that are divisible by 3 with no remainder */
@@ -24,6 +25,26 @@ namespace Iteration
             }
 
             Console.WriteLine(count);
+        }
+        /*summary
+         sum the numbers that the user entered*/
+        private static void Exercise2SumNumbers()
+        {
+            var count = 0;
+            while (true)
+            {
+                Console.WriteLine("Please enter a number");
+                var number = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(number))
+                {
+                    Console.WriteLine("The sum of the entered numbers is: " + count);
+                    break;
+                }
+                else
+                    Console.WriteLine("@Echo: " + number);
+                count += Convert.ToInt32(number);
+            }
         }
     }
 }
