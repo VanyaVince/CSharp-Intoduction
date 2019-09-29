0d418ae4db4c1953ca0be0f1ac568ad4fb8ad46a
vince <vanyavince@gmail.com>
2019-09-29 13:35:48 +0300

    summarize words second version

--- SummarizingText/Program.cs
+++ SummarizingText/Program.cs
@@ -11,21 +11,8 @@ namespace SummarizingText
         static void Main(string[] args)
         {
             var sentence = "We will look at a variety of different topics that are directly related to programming, and by the end, you should be able to answer questions like:";
-            Console.WriteLine(Summarize(sentence, 33));
-            
-        }
-
-        private static string Summarize(string str, int maxLength = 25)
-        {
-            var result = "";
-
-            if (str.Length < maxLength)
-                return $"Invalid value. The length of string are less then {maxLength}";
-
-            for (int i = 0; i < maxLength; i++)
-                result += str[i];
-
-            return result.Trim() + "...";
+            var summary = StringUtility.SummarizeText(sentence, 33);
+            Console.WriteLine(summary);
         }
     }
 }
---
+++ SummarizingText/StringUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummarizingText
+{
+    public class StringUtility
+    {
+        public static string SummarizeText(string str, int maxLength = 25)
+        {
+            var totalLengthOfWords = 0;
+            var wordsCount = 0;
+
+            if (str.Length <= maxLength)
+                return str.Trim() + "...";
+
+            var summaryWords = str.Split(' ');
+            foreach (var summaryWord in summaryWords)
+            {
+                wordsCount += 1;
+                totalLengthOfWords += summaryWord.Length;
+                if (totalLengthOfWords >= maxLength)
+                    break;
+            }
+
+            var summary = string.Join(" ", summaryWords , 0, wordsCount);
+            return summary + "...";
+        }
+    }
+}
