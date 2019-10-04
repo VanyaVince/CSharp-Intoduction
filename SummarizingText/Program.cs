e8a0f847bfdc4f6eb7925296932ab24c16849a93
vince <vanyavince@gmail.com>
2019-10-04 17:42:25 +0300

    add comment

--- SummarizingText/Program.cs
+++ SummarizingText/Program.cs
@@ -11,7 +11,7 @@ namespace SummarizingText
         static void Main(string[] args)
         {
             var sentence = "We will look at a variety of different topics that are directly related to programming, and by the end, you should be able to answer questions like:";
-            var summary = StringUtility.SummarizeText(sentence, 33);
+            var summary = StringUtility.SummarizeText(sentence, 25);
             Console.WriteLine(summary);
         }
     }
