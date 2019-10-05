8af0238538b1e0408b79d24ac40710ff349f3490
vince <vanyavince@gmail.com>
2019-10-05 12:59:55 +0300

    static and instance members

---
+++ C#IntermediateWithMosh/Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Classes
+{
+    public class Person
+    {
+        public String Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine($"Hi {to}, my Name is {Name}");
+        }
+
+        public static Person Parse(string name)
+        {
+            Person person = new Person();
+            person.Name = name;
+            return person;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Vince";
+            person.Introduce("Vanya");
+
+            var person1 = Person.Parse("Vince");
+            person1.Introduce("Vanya");
+        }
+    }
+}
