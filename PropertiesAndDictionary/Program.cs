889f1dbb2b28cb7a2fe5fb61867434549b81b3ec
vince <vanyavince@gmail.com>
2019-10-06 17:26:12 +0300

    dictionary

---
+++ PropertiesAndDictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesAndDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var school = new School(number: 150)
+            {
+                ["5A"] = "Marina", ["5B"] = "Keck", ["5C"] = "Sunny", ["5D"] = "Michael"
+            };
+
+            Console.WriteLine($"The name of 5A class teacher, school number of {school.Number} is {school["5A"]}");
+
+            Console.WriteLine();
+
+            school.ReadAllTeachers();
+
+            Console.WriteLine();
+            school["5A"] = "Mary";
+            Console.WriteLine($"The name of 5A class teacher, school number of {school.Number} is {school["5A"]}");
+
+            Console.WriteLine();
+
+            school.ReadAllTeachers();
+        }
+    }
+}
---
+++ PropertiesAndDictionary/School.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace PropertiesAndDictionary
+{
+    public class School
+    {
+        public int Number { get; set; }
+        private readonly Dictionary<string, string> _teachers;
+
+        public School()
+        {
+            _teachers = new Dictionary<string, string>();
+        }
+
+        public School(int number)
+            : this()
+        {
+            this.Number = number;
+        }
+
+        public string this[string key]
+        {
+            get { return _teachers[key];}
+            set { _teachers[key] = value; }
+        }
+
+        public void ReadAllTeachers()
+        {
+            foreach (var teacher in _teachers)
+                Console.WriteLine(teacher);
+        }
+    }
+}
