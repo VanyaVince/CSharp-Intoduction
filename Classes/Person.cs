168f6a0454068e674088d5dda4f951ec73096a06
vince <vanyavince@gmail.com>
2019-10-07 21:44:34 +0300

    rewriting code

---
+++ Classes/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Channels;
+
+namespace Classes
+{
+    public class Person
+    {
+        public String Name;
+        public int Age;
+        public readonly List<Orders> Orders = new List<Orders>();
+
+        public Person()
+        {
+        }
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public static Person Parse(string name)
+        {
+            Person person = new Person();
+            person.Name = name;
+            return person;
+        }
+        public void Introduce(string to)
+        {
+            Console.WriteLine($"Hi {to}, my Name is {Name}");
+        }
+
+        public void ShowYears(params int [] years)
+        {
+            Console.Write("The years when you got a champion ");
+            for (int i = 0; i < years.Length; i++)
+                Console.Write(years[i] + " ");
+            Console.WriteLine();
+        }
+
+        public static void Initialize(out int a)
+        {
+            a = 25;
+        }
+
+        public override string ToString()
+        {
+            return $"This is {this.Name}, {this.Age} years old";
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,34 +1,44 @@
 ﻿using System;
-
+using System.Runtime.InteropServices;
 
 namespace Classes
 {
-    public class Person
-    {
-        public String Name;
-
-        public void Introduce(string to)
-        {
-            Console.WriteLine($"Hi {to}, my Name is {Name}");
-        }
-
-        public static Person Parse(string name)
-        {
-            Person person = new Person();
-            person.Name = name;
-            return person;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
+
             Person person = new Person();
             person.Name = "Vince";
             person.Introduce("Vanya");
+            person.Orders.Add(new Orders());
+            person.Orders.Add(new Orders());
+            Console.WriteLine("The number of purchases that user did " + person.Orders.Count);
 
             var person1 = Person.Parse("Vince");
             person1.Introduce("Vanya");
+
+            person1.ShowYears(1999, 2000, 2005, 2010);
+
+            int age;
+            Person.Initialize(out age);
+            var person2 = new Person("Vanya", age);
+            Console.WriteLine(person2);
+        }
+
+        static void CheckingOfNumber()
+        {
+            var number = 0;
+            Console.WriteLine("Write a number");
+            var parse = int.TryParse(Console.ReadLine(), out number);
+            if (parse)
+            {
+                Console.WriteLine("Parse is completed successfully");
+            }
+            else
+            {
+                Console.WriteLine("Something goes wrong");
+            }
         }
     }
 }
