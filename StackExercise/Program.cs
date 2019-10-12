ed0373d8615634ae41052e340a97416a1d2b692a
vince <vanyavince@gmail.com>
2019-10-12 12:10:42 +0300

    exercise_Stack

---
+++ StackExercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StackExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+//            stack.Push(null);
+
+            for (int i = 0; i < 5; i++)
+                stack.Push("Object: " + (i + 1));
+
+            for (int i = 0; i < 5; i++)
+            {
+                var lastItem = stack.Pop();
+                Console.WriteLine(lastItem);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(1000);
+                stack.Push(DateTime.Now);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                var lastItem = stack.Pop();
+                Console.WriteLine(lastItem);
+            }
+        }
+    }
+}
---
+++ StackExercise/Stack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackExercise
+{
+    public class Stack
+    {
+        private readonly List<Object> _list;
+
+        public Stack()
+        {
+            _list = new List<object>();
+        }
+
+        public void Push(Object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("Object equals to null");
+
+            this._list.Insert(0, obj);
+        }
+
+        public Object Pop()
+        {
+            if (this._list.Count == 0)
+                throw new InvalidOperationException("List of object is empty");
+
+            var item = this._list[0];
+            this._list.Remove(item);
+
+            return item;
+        }
+
+        public void Clear()
+        {
+            this._list.Clear();
+        }
+    }
+}
