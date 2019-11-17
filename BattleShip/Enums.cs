eff480a7ea40210dd35dbafe908a0cb7b2822244
vince <vanyavince@gmail.com>
2019-11-18 00:23:49 +0300

    initial commit

---
+++ BattleShip/Enums.cs
@@ -0,0 +1,14 @@
+﻿namespace BattleShip
+{
+    public enum OccupationType
+    {
+        IsFree = '*',
+        IsNotAvailable,
+        Carrier = 'C',
+        Destroyer = 'D',
+        Submarine = 'S',
+        PatrolBoat = 'P',
+        Hit = 'X',
+        Miss = 'M'
+    }
+}
---
+++ BattleShip/Ships/Destroyer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShip.Ships
+{
+    public class Destroyer: Ship
+    {
+        public Destroyer()
+        {
+            this.Name = "Destroyer";
+            this.Size = 3;
+            this.Type = OccupationType.Destroyer;
+        }
+    }
+}
