8bead8656457ed58473ee37318965376280e1143
vince <vanyavince@gmail.com>
2020-07-30 15:50:47 +0300

    clean up the folder

--- CALCULATOR_OOP/CALCULATOR_OOP/Operation.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CALCULATOR_OOP
-{
-    public abstract class Operation
-    {
-        public object FirstArgument { get; set; }
-        public object SecondArgument { get; set; }
-        public object Result { get; set; }
-    }
-}
